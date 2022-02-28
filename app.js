9a57e7fdaf5e0c36aac6092b06da5dd12c4a773c
mahedi <mahedianik4@gmail.com>
2022-03-01 02:26:25 +0600

    main features , bonus part added

--- app.js
+++ app.js
@@ -49,23 +49,50 @@ const displayPhone = (phones) => {
 const getId = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
-    .then(res =>res.json())
-    .then(data => displayDetails(data.data))
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
 
 };
-const displayDetails =(product)=>{
-    console.log(product);
+
+const displayDetails = (product) => {
     const detailsDiv = document.getElementById('show-details');
+    detailsDiv.textContent = ''
     const div = document.createElement('div');
-    div.innerHTML =`
-    <div class="card border-0 shadow p-3 rounded mx-auto">
-    <img src="${product.image}" class="card-img-top w-50" alt="..." />
-        <div class="card-body">
-        <h5 class="card-title">${product.name}</h5>
-        <p class="card-text">${product.brand} </p>
-        </div>
+    div.classList.add("row", "shadow")
+    div.innerHTML = `
+    <div class="p-3  col-6 ">
+        <h3 class="card-text">${product.name} </h3>
+        <h5 class="text-danger">${product.releaseDate}</h5>
+        <h4 class="card-text">${product.brand} </h4>
+        <img src="${product.image}" class="card-img-top" alt=""/>
+    </div>
+    <div class=" p-3 col-6 ">
+         <ul class="list-group">
+            <h3 class="text-center text-info">Main Features</h3>
+            <li class="list-group-item"><h6 class="card-text">ChipSet: ${product.mainFeatures.chipSet} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Display: ${product.mainFeatures.displaySize} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Memory: ${product.mainFeatures.memory} </h6></li>
+         </ul>
+         
+         <ul class="list-group">
+            <h3 class="text-center text-info">Sensor Information</h3>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[0]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[1]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[2]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[3]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[4]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[5]} </h6></li>
+         </ul>
+
+         <ul class="list-group">
+            <h3 class="text-center text-info"> Others Information </h3>
+            <li class="list-group-item"><h6 class="card-text">Bluetooth: ${product.others?.Bluetooth} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">GPS: ${product.others?.GPS} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Radio: ${product.others?.Radio} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">USB: ${product.others?.USB} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">WLAN: ${product.others?.WLAN} </h6></li>
+         </ul>
     </div>
     `;
-    
     detailsDiv.appendChild(div)
 }
