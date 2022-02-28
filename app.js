d04b857e420999e9963729bac9140d40e82c6b8a
mahedi <mahedianik4@gmail.com>
2022-03-01 02:49:22 +0600

    responsive for mobile devise

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const getPhone = () => {
         .then(res => res.json())
         .then(data => displayPhone(data.data))
     document.getElementById('search-input').value = '';
-
 }
 
 // display phone function 
@@ -15,13 +14,11 @@ const displayPhone = (phones) => {
     if (phones.length == 0) {
         document.getElementById('input-error').style.display = "block"
     }
-
     else {
         document.getElementById('input-error').style.display = "none"
         const phonesDiv = document.getElementById('phones');
         // remove old search result
         phonesDiv.textContent = ''
-
         // get every phone by forEach 
         phones?.forEach(phone => {
             // console.log(phone)
@@ -29,23 +26,21 @@ const displayPhone = (phones) => {
             // add class div 
             div.classList.add("col-12", "col-lg-4")
             div.innerHTML = `
-                        <div class="card border-0 shadow p-3 rounded mx-auto" style="width:20rem">
-                        <img src="${phone.image}" class="card-img-top" alt="..." />
-                            <div class="card-body">
-                            <h5 class="card-title">${phone.phone_name}</h5>
-                            <p class="card-text">${phone.brand} </p>
-                            <button onclick="getId('${phone.slug}')" class="btn btn-primary">see more</button>
-                            </div>
-                        </div>
-                        `;
+            <div class="card border-0 shadow p-3 rounded mx-auto" style="width:20rem">
+            <img src="${phone.image}" class="card-img-top" alt="..." />
+                 <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand} </p>
+                    <button onclick="getId('${phone.slug}')" class="btn btn-primary">see more</button>
+                </div>
+            </div>
+            `;
             phonesDiv.appendChild(div)
-
         })
     }
 
-
 };
-
+// get product id dynamic url function
 const getId = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
@@ -54,28 +49,30 @@ const getId = (id) => {
 
 };
 
+
+// display product details function
 const displayDetails = (product) => {
     const detailsDiv = document.getElementById('show-details');
-    detailsDiv.textContent = ''
+    detailsDiv.textContent = '';
     const div = document.createElement('div');
-    div.classList.add("row", "shadow")
+    div.classList.add("row","shadow")
     div.innerHTML = `
-    <div class="p-3  col-6 ">
+    <div class="p-3 col-12  col-lg-6 ">
         <h3 class="card-text">${product.name} </h3>
         <h5 class="text-danger">${product.releaseDate}</h5>
-        <h4 class="card-text">${product.brand} </h4>
+        <h4 class="card-text"> Brand: ${product.brand} </h4>
         <img src="${product.image}" class="card-img-top" alt=""/>
     </div>
-    <div class=" p-3 col-6 ">
+    <div class=" p-3 col-12 col-lg-6 ">
          <ul class="list-group">
-            <h3 class="text-center text-info">Main Features</h3>
+            <h5 class="text-center text-info">Main Features</h5>
             <li class="list-group-item"><h6 class="card-text">ChipSet: ${product.mainFeatures.chipSet} </h6></li>
             <li class="list-group-item"><h6 class="card-text">Display: ${product.mainFeatures.displaySize} </h6></li>
             <li class="list-group-item"><h6 class="card-text">Memory: ${product.mainFeatures.memory} </h6></li>
          </ul>
          
          <ul class="list-group">
-            <h3 class="text-center text-info">Sensor Information</h3>
+            <h5 class="text-center text-info">Sensor Information</h5>
             <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[0]} </h6></li>
             <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[1]} </h6></li>
             <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[2]} </h6></li>
@@ -85,7 +82,7 @@ const displayDetails = (product) => {
          </ul>
 
          <ul class="list-group">
-            <h3 class="text-center text-info"> Others Information </h3>
+            <h5 class="text-center text-info"> Others Information </h5>
             <li class="list-group-item"><h6 class="card-text">Bluetooth: ${product.others?.Bluetooth} </h6></li>
             <li class="list-group-item"><h6 class="card-text">GPS: ${product.others?.GPS} </h6></li>
             <li class="list-group-item"><h6 class="card-text">Radio: ${product.others?.Radio} </h6></li>
@@ -94,5 +91,5 @@ const displayDetails = (product) => {
          </ul>
     </div>
     `;
-    detailsDiv.appendChild(div)
-}
+    detailsDiv.appendChild(div);
+};
